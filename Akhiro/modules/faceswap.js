9a42ade8275138297753f28230ca552e17ac59b9
certainlyakhiro <165024882+certainlyakhiro@users.noreply.github.com>
2024-04-17 15:04:01 +0000

    Added faceswap

--- Akhiro/modules/faceswap.js
+++ Akhiro/modules/faceswap.js
@@ -1,39 +1,62 @@
+const axios = require('axios');
+const fs = require('fs-extra');
+
 module.exports = {
   metadata: {
     name: "faceswap",
-    role: 0,
-    author: "AkhiroDEV | Samir",
+    author: "AkhiroDEV",
     hasPrefix: false,
-    description: "Face swap faces",
-    usage: "faceswap"
+    botAdmin: true,
+    description: "Faceswap your face into different faces",
+    usage: "faceswap [ reply with two pictures ]"
   },
-  onRun: async function({ args, event, api }) {
-    try {
-      if (event.type != "message_reply") {
-        return api.sendMessage("Please reply to a message with two images attached.", event.threadID);
+  async onRun({ api, event }) {
+      const args = event.body.split(/\s+/);
+  args.shift(); 
+
+  const reply = (message) => api.sendMessage(message, event.threadID, event.messageID);
+
+  if (event.type === "message_reply") {
+    const attachments = event.messageReply.attachments.filter(attachment => attachment.type === "photo");
+
+    if (attachments.length >= 2) {
+      const [url1, url2] = attachments.map(attachment => attachment.url);
+      const path = __dirname + `/cache/swapped_image.jpg`;
+
+      api.sendMessage("🔮 | 𝙿𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝 𝚠𝚑𝚒𝚕𝚎 𝚠𝚎 𝚜𝚠𝚊𝚙 𝚢𝚘𝚞𝚛 𝚒𝚖𝚊𝚐𝚎𝚜...", event.threadID, event.messageID);
+
+      try {
+        const response = await axios.get('https://haze-faceswap.replit.app/swap', {
+          params: {
+            swap_image: url1,
+            target_image: url2
+          }
+        });
+
+        const processedImageURL = response.data.hazeswap;
+        const { data } = await axios.get(processedImageURL, { responseType: "stream" });
+
+        const writer = fs.createWriteStream(path);
+        data.pipe(writer);
+
+        writer.on('finish', () => {
+          api.sendMessage({
+            body: "🔮 𝙸𝚖𝚊𝚐𝚎 𝚂𝚠𝚊𝚙 𝚂𝚞𝚌𝚌𝚎𝚜𝚜𝚏𝚞𝚕𝚕𝚢",
+            attachment: fs.createReadStream(path)
+          }, event.threadID, (err, messageInfo) => {
+            if (err) {
+              reply("🤖 𝙴𝚛𝚛𝚘𝚛 𝚜𝚎𝚗𝚍𝚒𝚗𝚐 𝚖𝚎𝚜𝚜𝚊𝚐𝚎: " + err);
+            } else {
+              fs.unlinkSync(path);
+            }
+          });
+        });
+      } catch (error) {
+        reply(`🤖 𝙿𝚛𝚘𝚌𝚎𝚜𝚜𝚎𝚜𝚒𝚗𝚐 𝚒𝚖𝚊𝚐𝚎𝚜: ${error}`);
       }
-
-      let links = [];
-      for (let attachment of event.messageReply.attachments) {
-        links.push(attachment.url);
-      }
-
-      if (links.length < 2) {
-        return api.sendMessage("Please ensure there are exactly two images attached.", event.threadID);
-      }
-
-      const shortLink1 = await global.utils.uploadImgbb(links[0]);
-      const Url1 = shortLink1.image.url;
-
-      const shortLink2 = await global.utils.uploadImgbb(links[1]);
-      const Url2 = shortLink2.image.url;
-
-      let swapface = `https://apis-samir.onrender.com/faceswap?sourceUrl=${Url1}&targetUrl=${Url2}`;
-      const stream = await global.utils.getStreamFromURL(swapface);
-      api.sendMessage({ attachment: stream }, event.threadID);
-    } catch (error) {
-      console.error(error);
-      api.sendMessage(`An error occurred while processing the face swap. ${error.message}`, event.threadID);
-    }
-  }
-};
+    } else {
+      reply("🔮 𝙵𝚊𝚌𝚎 𝚂𝚠𝚊𝚙\n\n𝚄𝚜𝚊𝚐𝚎: 𝚜𝚠𝚊𝚙 [ 𝚛𝚎𝚙𝚕𝚢 1 𝚊𝚗𝚍 2 𝚒𝚖𝚊𝚐𝚎 ]");
+     }
+   }
+ }
+}
